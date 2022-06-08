c76ead6927a1e312cf5b248400d58ec24cca4f68
rajeshwar reddy molakala <rajeshwar11296@hotmail.com>
2022-06-08 11:33:19 +0530

    app component changed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,15 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-<<<<<<< HEAD
   constructor(){}
 }
-=======
-  constructor(private _router: Router) { }
-  navigatetosecond: any = '/secondcomponent';
-
-  GotoSecondComponent(navigatetosecond: any) {
-    this._router.navigate([navigatetosecond]);
-  }
-}
->>>>>>> 5ece6559e29405430095904678cab95de41223d0
