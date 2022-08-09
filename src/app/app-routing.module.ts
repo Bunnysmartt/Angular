013f0a7c1247b3563fe6c8c584475934c2454208
Bunny <ravikotipalli2020@gmail.com>
2022-08-09 12:44:49 +0530

    Reverted all components and added new sample component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { FirstComponent } from './About/about.component';
-import { SecondComponent } from './Form/form.component';
-import { UserDetailsComponent } from './User-Details/user-details.component';
 
-const routes: Routes = [
-  { path:'about', component: FirstComponent}, 
-  { path: 'form', component: SecondComponent},
-  { path: 'user-details', component: UserDetailsComponent},
-  { path: '', redirectTo:'app', pathMatch:'full'}];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
-import { SecondComponent } from './Form/form.component';
-import { FirstComponent } from './About/about.component';
-import { UserDetailsComponent } from './User-Details/user-details.component';
+import { SampleComponent } from './sample/sample.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SecondComponent,
-    FirstComponent,
-    UserDetailsComponent
-  ],
+    SampleComponent
+    ],
   imports: [
     BrowserModule,
     AppRoutingModule,
